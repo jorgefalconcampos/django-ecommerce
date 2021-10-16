c08c80ea0291da8189c043cdf3dcf6c218d57630
Jorge Falcón <jorgef_campos@outlook.com>
2021-10-16 18:06:18 -0500

    pre_save callback for slug

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
 class Product(models.Model):
@@ -10,11 +11,12 @@ class Product(models.Model):
     slug = models.SlugField(null=False, blank=False, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def save(self, *args, **kwargs):
-        """overrides the save method"""
-        self.slug = slugify(self.title)
-        super(Product, self).save(*args, **kwargs)
-
     def __str__(self):
         """changes the title"""
         return str(self.title)
+
+
+def create_slug(sender, instance, *args, **kwargs):
+    instance.slug = slugify(instance.title)
+
+pre_save.connect(create_slug, sender=Product)
