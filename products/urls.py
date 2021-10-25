21e3cc20913e1ba818b1d5f2f827a41d1ac2bb65
Jorge Falcón <jorgef_campos@outlook.com>
2021-10-24 22:56:23 -0500

    created fixture

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'products' # routing for this app only
+
 urlpatterns = [
     path('search', views.ProductSearchListView.as_view(), name="search"),
     path('<slug:slug>', views.ProductDetailView.as_view(), name="product")
