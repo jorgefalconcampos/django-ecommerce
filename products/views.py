e2c09c3c8775cc065ecbf9beea20d049e96a0954
Jorge Falcón <jorgef_campos@outlook.com>
2021-10-24 19:34:26 -0500

    Added snippets and search functionality

--- products/views.py
+++ products/views.py
@@ -11,7 +11,6 @@ TEMPLATES_APP_PRODUCTS = settings.TEMPLATES_DIR_APP_PRODUCTS #importing TEMPLATE
 
 # Create your views here.
 class ProductListView(ListView):
-
     template_name = TEMPLATES / 'main' / 'index.html'
     queryset = Product.objects.all().order_by('-id')
 
@@ -25,3 +24,22 @@ class ProductDetailView(DetailView): # id => pk (by default)
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class ProductSearchListView(ListView):
+    template_name = TEMPLATES_APP_PRODUCTS / 'products' / 'search.html'
+    
+    def get_queryset(self):
+        return Product.objects.filter(title__icontains=self.query())
+
+    def query(self):
+        return self.request.GET.get('q')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        context['count'] = context['product_list' ].count()
+        
+        return context
+
+
