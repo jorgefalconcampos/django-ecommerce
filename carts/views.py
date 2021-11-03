5aaf27f83672da2b1137190b3c3196421fc6429d
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-02 22:57:59 -0600

    Cart session

--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
-
+from . models import Cart
+from . utils import get_or_create_cart
 
 TEMPLATES = settings.TEMPLATES_DIR
 TEMPLATES_APP_CARTS = settings.TEMPLATES_DIR_APP_CART #importing TEMPLATES_APP_PRODUCTS path from Django settings
@@ -9,5 +10,7 @@ TEMPLATES_APP_CARTS = settings.TEMPLATES_DIR_APP_CART #importing TEMPLATES_APP_P
 # Create your views here.
 def cart(request):
     template_name = TEMPLATES_APP_CARTS / 'carts' / 'cart.html'
-    print(f"\n\n\n {template_name}" )
+
+    cart = get_or_create_cart(request)
+
     return render (request, template_name)
