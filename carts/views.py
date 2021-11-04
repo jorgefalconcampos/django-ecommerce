613d000ef8998b102bc7b9ae9fe77e102194a3ed
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-03 23:00:30 -0600

    Add to cart functionality

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.conf import settings
 from . models import Cart
 from . utils import get_or_create_cart
+from . models import Product
 
 TEMPLATES = settings.TEMPLATES_DIR
 TEMPLATES_APP_CARTS = settings.TEMPLATES_DIR_APP_CART #importing TEMPLATES_APP_PRODUCTS path from Django settings
@@ -14,3 +15,16 @@ def cart(request):
     cart = get_or_create_cart(request)
 
     return render (request, template_name)
+
+def add(request):
+
+    template_name = TEMPLATES_APP_CARTS / 'carts' / 'add.html'
+
+
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+    cart.products.add(product)
+
+    context = { 'product': product }
+
+    return render(request, template_name, context)
