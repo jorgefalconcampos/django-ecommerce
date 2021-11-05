ccdade3b054629834fc1574e7434c215d5948056
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-04 23:35:46 -0600

    Remove product from cart

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from . models import Cart
 from . utils import get_or_create_cart
 from . models import Product
+from django.shortcuts import redirect
+
 
 TEMPLATES = settings.TEMPLATES_DIR
 TEMPLATES_APP_CARTS = settings.TEMPLATES_DIR_APP_CART #importing TEMPLATES_APP_PRODUCTS path from Django settings
@@ -14,17 +16,25 @@ def cart(request):
 
     cart = get_or_create_cart(request)
 
-    return render (request, template_name)
+    context = {
+        'cart': cart
+    }
+
+    return render (request, template_name, context)
 
 def add(request):
-
     template_name = TEMPLATES_APP_CARTS / 'carts' / 'add.html'
-
-
     cart = get_or_create_cart(request)
     product = Product.objects.get(pk=request.POST.get('product_id'))
     cart.products.add(product)
-
     context = { 'product': product }
-
     return render(request, template_name, context)
+
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+
+    cart.products.remove(product)
+
+    return redirect('carts:cart')
+
