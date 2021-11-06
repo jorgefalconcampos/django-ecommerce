8ad141d0d7a70ed1bdb7a3bdc68d24fc83fb5102
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-06 15:15:35 -0600

    Filters and totals

--- carts/models.py
+++ carts/models.py
@@ -1,7 +1,7 @@
 import uuid
 import decimal
 from django.db import models
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.db.models.signals import m2m_changed
 from users.models import User
 from products.models import Product
@@ -26,12 +26,29 @@ class Cart(models.Model):
         self.update_subtotals()
 
     def update_subtotals(self):
-        self.subtotal = sum([product.price for product in self.products.all()])
+        self.subtotal = sum([
+            cp.quantity * cp.product.price for cp in self.products_related()
+        ])
         self.save()
 
     def update_total(self):
         self.total = self.subtotal + (self.subtotal * decimal.Decimal(Cart.FEE))
         self.save()
+
+    def products_related(self):
+        return self.cartproducts_set.select_related('product')
+
+
+class CartProductsManager(models.Manager):
+
+    def create_or_update_qtty(self, cart, product, quantity=1):
+        object, created = self.get_or_create(cart=cart, product=product)
+
+        if not created:
+            quantity = object.quantity +  quantity
+            
+        object.update_qtty(quantity)
+        return object
 
 
 class CartProducts(models.Model):
@@ -40,6 +57,11 @@ class CartProducts(models.Model):
     quantity = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    objects = CartProductsManager()
+
+    def update_qtty(self, quantity=1):
+        self.quantity = quantity
+        self.save()
 
 
 
@@ -51,7 +73,13 @@ def update_totals(sender, instance, action, *args, **kwargs):
     if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
         instance.update_totals()
 
+def post_save_update_totals(sender, instance, *args, **kwargs):
+    instance.cart.update_totals()
+
+
+
 
 
 pre_save.connect(set_cart_id, sender=Cart)
+post_save.connect(post_save_update_totals, sender=CartProducts)
 m2m_changed.connect(update_totals, sender=Cart.products.through)
--- carts/views.py
+++ carts/views.py
@@ -5,7 +5,7 @@ from . utils import get_or_create_cart
 from . models import Product
 from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
-
+from .models import CartProducts
 
 TEMPLATES = settings.TEMPLATES_DIR
 TEMPLATES_APP_CARTS = settings.TEMPLATES_DIR_APP_CART #importing TEMPLATES_APP_PRODUCTS path from Django settings
@@ -25,10 +25,22 @@ def cart(request):
 
 def add(request):
     template_name = TEMPLATES_APP_CARTS / 'carts' / 'add.html'
+
     cart = get_or_create_cart(request)
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    cart.products.add(product)
-    context = { 'product': product }
+    quantity = int(request.POST.get('quantity', 1))
+
+    # cart.products.add(product, through_defaults={
+    #     'quantity': quantity
+    # })
+    cart_product = CartProducts.objects.create_or_update_qtty(cart=cart, product=product, quantity=quantity)
+
+   
+    context = { 
+        'quantity': quantity,
+        'cart_product': cart_product,
+        'product': product
+    }
     return render(request, template_name, context)
 
 def remove(request):
