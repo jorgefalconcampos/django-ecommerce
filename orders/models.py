99db7354a84e1dd7112f167252acf69f351a28d3
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-14 23:43:59 -0600

    defined order id field

--- orders/models.py
+++ orders/models.py
@@ -1,8 +1,9 @@
+import uuid
 from enum import Enum
 from django.db import models
 from users.models import User
 from carts.models import Cart
-
+from django.db.models.signals import pre_save
 
 class OrderStatus(Enum):
     CREATED = 'CREATED'
@@ -15,6 +16,7 @@ choices = [(tag, tag.value) for tag in OrderStatus]
 
 # Create your models here.
 class Order(models.Model):
+    order_id = models.CharField(max_length=100, null=False, blank=False, unique=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     status = models.CharField(max_length=50, choices=choices, default=OrderStatus.CREATED)
@@ -25,4 +27,11 @@ class Order(models.Model):
     
 
     def __str__(self): 
-        return ''
+        return self.order_id
+
+
+def set_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id = str(uuid.uuid4())
+
+pre_save.connect(set_order_id, sender=Order)
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from carts.utils import get_or_create_cart
+from . models import Order
+from . utils import get_or_create_order
+
 
 # Create your views here.
 def order (request):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart, request)
+
+
     return render(request, 'orders/order.html', {
         
     })
