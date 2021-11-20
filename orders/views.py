1bdae60dd4b518ac2bbaea24a5d5168d8f64293e
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-19 21:17:04 -0600

    select address for current order

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,12 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from carts.utils import get_or_create_cart
 from . models import Order
 from . utils import get_or_create_order, breadcrumb
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from . models import Order
+from shipping_addresses.models import ShippingAdresses
 
 # Create your views here.
 
@@ -18,6 +22,7 @@ def order (request):
         'breadcrumb': breadcrumb()
     })
 
+@login_required(login_url='login')
 def address(request):
     cart = get_or_create_cart(request)
     order = get_or_create_order(cart, request)
@@ -30,3 +35,26 @@ def address(request):
         'shipping_address': shipping_address,
         'breadcrumb': breadcrumb(address=True)
     })
+
+@login_required(login_url='login')
+def select_address(request):
+    shipping_addresses = request.user.shippingadresses_set.all()
+
+    return render(request, 'orders/select_address.html', {
+        'breadcrumb': breadcrumb(address=True),
+        'shipping_addresses': shipping_addresses
+    })
+
+
+@login_required(login_url='login')
+def check_address(request, pk):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart, request)
+    shipping_address = get_object_or_404(ShippingAdresses, pk=pk)
+
+    if request.user.id != shipping_address.user_id:
+        return redirect('carts:cart')
+
+    order.update_shipping_address(shipping_address)
+
+    return redirect('orders:address')
