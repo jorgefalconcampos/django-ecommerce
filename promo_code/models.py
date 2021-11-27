2f8f0d4fba5f07ed94623dc85ed08b91a8d4c6ff
Jorge Falcón <jorgef_campos@outlook.com>
2021-11-26 22:00:53 -0600

    validate promo code

--- promo_code/models.py
+++ promo_code/models.py
@@ -1,10 +1,22 @@
 import string
 import random
-
+from django.utils import timezone
 from django.db import models
 from django.db.models.signals import pre_save
 
 # Create your models here.
+
+
+class PromoCodeManager(models.Manager):
+
+    """Validates the"""
+    def get_valid(self, code):
+        now = timezone.now()
+
+        return self.filter(code=code).filter(used=False).filter(valid_from__lte=now).filter(valid_to__gte=now).first()
+
+
+
 class PromoCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
     discount = models.FloatField(default=0.0)
@@ -12,6 +24,8 @@ class PromoCode(models.Model):
     valid_to = models.DateField()
     used = models.BooleanField(default=False)
     created_at = models.DateField(auto_now_add=True)
+
+    objects = PromoCodeManager()
 
     def __str__(self):
         return self.code
